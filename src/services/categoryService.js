240131215bd68a1ef6da3f07e555a67e1bbb121a
andresjpg2000 <andresjpg2000@gmail.com>
2025-12-16 16:50:31 +0000

    added prisma accelerate to speed up queries

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,7 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import prisma from "../config/db.js";
 import logger from "../utils/logger.js";
-
-const prisma = new PrismaClient();
 
 export const getAllCategories = async () => {
   logger.info("Fetching all categories");
--- src/services/itemService.js
+++ src/services/itemService.js
@@ -1,7 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import prisma from "../config/db.js";
 import logger from "../utils/logger.js";
-
-const prisma = new PrismaClient();
 
 export const getAllItems = async () => {
   logger.info("Fetching all items");
